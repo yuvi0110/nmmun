9e4e83a164bea15f8863e5034f1cbd5cf3f37089
Clupai8o0 <clupaio4@gmail.com>
2023-07-22 10:38:16 +0300

    minimized the classes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
+	return (
 		<section className="bg-blue-700 text-white rounded-2xl flex flex-col justify-center items-center py-12 md:py-20 mt-6">
 			{/* TITLE */}
 			<h1 className="title">NMMUN</h1>
@@ -11,39 +11,39 @@ const Hero = () => {
 
 			{/* COUNTDOWN + DATE */}
 			<div className="grid grid-flow-col gap-2 md:gap-4 text-center auto-cols-max my-4 md:my-6">
-				<div className="flex flex-col p-2 bg-blue-900 rounded-xl">
-					<span className="countdown font-mono text-4xl md:text-5xl font-medium">
+				<div className="timer-container">
+					<span className="number-span">
 						<span>10</span>
 					</span>
-					<span className="text-sm uppercase opacity-80 font-medium">days</span>
+					<span className="detail-span">days</span>
 				</div>
-				<div className="flex flex-col p-2 bg-blue-900 rounded-xl">
-					<span className="countdown font-mono text-4xl md:text-5xl font-medium">
+				<div className="timer-container">
+					<span className="number-span">
 						<span>10</span>
 					</span>
-					<span className="text-sm uppercase opacity-80 font-medium">hours</span>
+					<span className="detail-span">hours</span>
 				</div>
-				<div className="flex flex-col p-2 bg-blue-900 rounded-xl">
-					<span className="countdown font-mono text-4xl md:text-5xl font-medium">
+				<div className="timer-container">
+					<span className="number-span">
 						<span>10</span>
 					</span>
-					<span className="text-sm uppercase opacity-80 font-medium">min</span>
+					<span className="detail-span">min</span>
 				</div>
-				<div className="flex flex-col p-2 bg-blue-900 rounded-xl">
-					<span className="countdown font-mono text-4xl md:text-5xl font-medium">
+				<div className="timer-container">
+					<span className="number-span">
 						<span>10</span>
 					</span>
-					<span className="text-sm uppercase opacity-80 font-medium">sec</span>
+					<span className="detail-span">sec</span>
 				</div>
 			</div>
 
 			{/* CTA */}
-			<div className='flex gap-2 md:gap-4 mt-4 md:mt-6'>
-				<button className="font-semibold bg-white px-6 py-2 text-black rounded-md">Contact Us</button>
-				<button className="font-semibold bg-white px-6 py-2 text-black rounded-md">Apply</button>
+			<div className="flex gap-2 md:gap-4 mt-4 md:mt-6">
+				<button className="hero-btn">Contact Us</button>
+				<button className="hero-btn">Apply</button>
 			</div>
 		</section>
 	);
-}
+};
 
-export default Hero
+export default Hero;
