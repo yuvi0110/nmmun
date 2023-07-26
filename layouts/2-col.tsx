26afae6c54a13b34cb8c2a5efcd2bd5af8b109b5
Clupai8o0 <clupaio4@gmail.com>
2023-07-26 10:02:32 +0300

    mobile spacing felt off so

--- layouts/2-col.tsx
+++ layouts/2-col.tsx
@@ -12,7 +12,7 @@ function TwoColumnsLayout({
 		<section className="w-full flex justify-center p-h py-16">
 			<div className="max-w-7xl w-full">
 				<Heading className="mb-8">{title}</Heading>
-				<div className="flex flex-col md:flex-row">{children}</div>
+				<div className="flex flex-col md:flex-row gap-8">{children}</div>
 			</div>
 		</section>
 	);
